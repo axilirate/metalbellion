b29d3013a3b69f4c8837d86e9610b746cea9e7b6
axilirate <56825186+axilirate@users.noreply.github.com>
2023-04-29 15:02:22 +0300

    update

---
+++ scenes/cameras/player_camera/player_camera.gd
@@ -0,0 +1,21 @@
+class_name PlayerCamera extends Camera3D
+
+
+
+
+
+
+
+
+
+
+
+func _input(event: InputEvent) -> void:
+	process_look_rotation(event)
+
+
+
+
+
+func process_look_rotation(event: InputEvent) -> void:
+	pass
